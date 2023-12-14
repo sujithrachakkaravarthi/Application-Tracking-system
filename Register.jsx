430324cd7130aa9c44d4e2b2fc0719a1392aa5cd
sujithrachakkaravarthi <122348706+sujithrachakkaravarthi@users.noreply.github.com>
2023-12-14 10:04:25 +0530

    Add files via upload

---
+++ Register.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+export const Register = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>SIGN UP</h2>
+        <form className="register-form" onSubmit={handleSubmit}>
+            <label htmlFor="name">Full Name</label>
+            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
+            <label htmlFor="email">Email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <label htmlFor="password">Password</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="****" id="password" name="password" />
+            <label htmlFor="password">Confirm Password</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="****" id="password" name="password" />
+            
+            <button type="submit">SIGN IN</button>
+        </form>
+        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already Have Account? Login here.</button>
+    </div>
+    )
+}
